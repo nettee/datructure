f24e491eec8f648509f635a5d9415192f239a705
William <xf0718@gmail.com>
2014-09-24 09:32:24 +0800

    improve data inputting

--- list/odd-even.c
+++ list/odd-even.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
 void rearrange(int *v, int len);
 
 int main()
 {
-    int v[] = {3, 5, 2, 4, 9, 8, 1, 6, 7, 10, 11};
-    rearrange(v, 11);
+    int size;
     int i;
+    scanf("%d", &size);
+    int *v = (int *)malloc(size * sizeof(int));
+    for (i = 0; i < size; i++) {
+        scanf("%d", v + i);
+    }
+    
+    //int v[] = {3, 5, 2, 4, 9, 8, 1, 6, 7, 10, 11};
+    rearrange(v, size);
     for (i = 0; i < 11; i++) {
         printf("%d ", v[i]);
     }
