aa3c12b192ccb8e40866990ca743da83e14c7a25
William <xf0718@gmail.com>
2014-09-27 16:45:40 +0800

    new dlist

---
+++ list/utest_dlist.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "dlist.h"
+
+using namespace std;
+
+int main()
+{
+    dlist<int> a;
+    assert(a.empty());
+    assert(a.size() == 0);
+    a.push_front(1);
+    a.push_back(8);
+    a.push_back(17);
+    a.push_back(80);
+    assert(!a.empty());
+    assert(a.size() == 4);
+    a.setitem(1, 9);
+    assert(a.getitem(1) == 9);
+    assert(a.size() == 4);
+    a.pop_front();
+    a.pop_front();
+    assert(a.size() == 2);
+
+}
+
+
