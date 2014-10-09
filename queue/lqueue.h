0881f4482b92e950c797be77577e8b19a23f6e03
William <xf0718@gmail.com>
2014-10-09 20:58:13 +0800

    new queue files

---
+++ queue/lqueue.h
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cassert>
+
+#include "dlist.h"
+
+#ifndef _LQUEUE_H_
+#define _LQUEUE_H_
+
+template <typename T>
+class lqueue {
+public:
+    lqueue();
+    ~lqueue();
+
+    bool empty()const;
+    int size()const;
+
+    void enqueue(T x);
+    T dequeue();
+
+private:
+    dlist<T> queue;
+};
+
+template <typename T>
+lqueue<T>::lqueue()
+{
+    queue = dlist<T>();
+}
+
+template <typename T>
+lqueue<T>::~lqueue()
+{
+    queue.~dlist();
+}
+
+template <typename T>
+bool lqueue<T>::empty()const
+{
+    return queue.empty();
+}
+ 
+template <typename T>
+int lqueue<T>::size()const
+{
+    return queue.size();
+}
+
+template <typename T>
+void lqueue<T>::enqueue(T x)
+{
+    queue.push_back(x);
+}
+
+template <typename T>
+T lqueue<T>::dequeue()
+{
+    return queue.pop_front();
+}
+
+
+
+
+
+
+#endif 
---
+++ queue/utest_lqueue.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lqueue.h"
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
