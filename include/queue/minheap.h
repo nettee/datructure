f8ccc2d8dc8978003b7d8d6f809b9b9f5a58905f
William <xf0718@gmail.com>
2014-11-12 23:59:07 +0800

    minheap - basic insert and delete

--- include/queue/minheap.h
+++ include/queue/minheap.h
@@ -1,29 +1,37 @@
+#include <iostream>
 #include <cassert>
 
 template <typename T>
 class MinHeap {
 public:
-    MinHeap(int msz = 100);
+    MinHeap(T sentinel, int msz = 100);
     ~MinHeap();
     void clear();
 
     bool empty();
     bool full();
+    int size();
 
     void insert(T x);
+    T pop_min();
+
+    void input(T *v, int len);
+    void output();
 
 private:
     T *v;
+    T sentinel;
     int siz;
     int msiz;
 };
 
 template <typename T>
-MinHeap<T>::MinHeap(int msz)
+MinHeap<T>::MinHeap(T sentinel, int msz)
 {
-    v = new T[msz];
+    v = new T[msz+1];
     msiz = msz;
     siz = 0;
+    v[0] = sentinel;
 }
 
 template <typename T>
@@ -52,9 +60,62 @@ bool MinHeap<T>::full()
 }
 
 template <typename T>
+int MinHeap<T>::size()
+{
+    return siz;
+}
+
+template <typename T>
 void MinHeap<T>::insert(T x)
 {
     assert(!full());
-    v[siz] = x;
-    ++siz;
+    v[++siz] = x;
+    int i = siz;
+    while (v[i/2] > x) {
+        v[i] = v[i/2];
+        i /= 2;
+    }
+    v[i] = x;
+}
+
+template <typename T>
+T MinHeap<T>::pop_min()
+{
+    assert(!empty());
+    T min_val = v[1];
+    T last_element = v[siz--];
+    int i = 1;
+    while (i * 2 <= siz) {
+        int child = i * 2;
+        if (child != siz && v[child+1] < v[child]) {
+            ++child;
+        }
+        if (last_element > v[child]) {
+            v[i] = v[child];
+        } else {
+            break;
+        }
+        i = child;
+    }
+    v[i] = last_element;
+    return min_val;
+}
+
+template <typename T>
+void MinHeap<T>::input(T *xs, int len)
+{
+    assert(len <= msiz);
+    for (int i = 0; i < len; i++) {
+        v[i+1] = xs[i];
+    }
+    siz = len;
+}
+
+template <typename T>
+void MinHeap<T>::output()
+{
+    for (int i = 1; i <= siz; i++) {
+       std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
 }
--- test/minheap.cpp
+++ test/minheap.cpp
@@ -2,8 +2,13 @@
 
 using namespace std;
 
+int xs[] = {13, 14, 16, 19, 21, 19, 68, 65, 26, 32, 31};
+
 int main()
 {
-    MinHeap<int> h;
+    MinHeap<int> h(-1);
+    h.input(xs, 10);
+    h.pop_min();
+    h.output();
     return 0;
 }
