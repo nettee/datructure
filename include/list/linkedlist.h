eb6afefafb33a4cbaf806fdf4409194b2f296ac0
William <xf0718@gmail.com>
2015-01-22 23:44:40 +0800

    modify linkedlist

--- include/list/linkedlist.h
+++ include/list/linkedlist.h
@@ -1,26 +1,16 @@
 #ifndef __LINKED_LIST_H__
 #define __LINKED_LIST_H__
 
-#include "list/array.h"
-
-#include <iostream>
 #include <cassert>
+#include "list/array.h"
 
 template <typename E>
 struct LinkedNode {
-    E val;
     LinkedNode<E> *prev;
     LinkedNode<E> *next;
-    LinkedNode() {
-        val = E();
-        prev = NULL;
-        next = NULL;
-    }
-    LinkedNode(E x) {
-        val = x;
-        prev = NULL;
-        next = NULL;
-    }
+    E val;
+    LinkedNode(): prev(NULL), next(NULL), val(E()) {}
+    LinkedNode(const E& x): prev(NULL), next(NULL) { val = x; }
 };
 
 template <typename E>
@@ -31,7 +21,7 @@ public:
         rear = new LinkedNode<E>();
         head->next = rear;
         rear->prev = head;
-        theSize = 0;
+        siz = 0;
     }
 
     ~LinkedList() {
@@ -42,52 +32,27 @@ public:
 
     void clear() {
         LinkedNode<E> *q = head->next;
-        for (int i = 0; i < theSize; i++) {
+        for (int i = 0; i < siz; i++) {
             LinkedNode<E> *p = q;
             q = q->next;
             delete p;
         }
         head->next = rear;
         rear->prev = head;
-        theSize = 0;
+        siz = 0;
     }
 
-    bool isEmpty() {
-        return theSize == 0;
-    }
+    bool empty() const { return siz == 0; }
+    int size() const { return siz; }
 
-    int size() {
-        return theSize;
-    }
-
-    E getFirst() {
+    E first() {
         assert(!isEmpty());
         return head->next->val;
     }
 
-    E getLast() {
+    E last() {
         assert(!isEmpty());
         return rear->prev->val;
-    }
-
-    E get(int index) {
-        assert(index >= 0 && index < theSize);
-        LinkedNode<E> *q = head->next;
-        for (int j = 0; j < index; j++) {
-            q = q->next;
-        }
-        return q->val;
-    }
-
-    E set(int index, E e) {
-        assert(index >= 0 && index < theSize);
-        LinkedNode<E> *q = head->next;
-        for (int j = 0; j < index; j++) {
-            q = q->next;
-        }
-        E prev_element = q->val;
-        q->val = e;
-        return prev_element;
     }
 
     bool contains(E x) {
@@ -102,7 +67,7 @@ public:
 
     int indexOf(E x) {
         LinkedNode<E> *q = head->next;
-        for (int i = 0; i < theSize; i++) {
+        for (int i = 0; i < siz; i++) {
             if (q->val == x) {
                 return i;
             }
@@ -114,7 +79,7 @@ public:
     int lastIndexOf(E x) {
         int lastIndex = -1;
         LinkedNode<E> *q = head->next;
-        for (int i = 0; i < theSize; i++) {
+        for (int i = 0; i < siz; i++) {
             if (q->val == x) {
                 lastIndex = i;
             }
@@ -123,188 +88,57 @@ public:
         return lastIndex;
     }
 
-    void addFirst(E e) {
+    void push_front(const E& e) {
         LinkedNode<E> *q = new LinkedNode<E>(e);
         q->next = head->next;
         head->next->prev = q;
         q->prev = head;
         head->next = q;
-        ++theSize;
+        ++siz;
     }
 
-    void addLast(E e) {
+    void push_back(const E& e) {
         LinkedNode<E> *q = new LinkedNode<E>(e);
         q->prev = rear->prev;
         rear->prev->next = q;
         q->next = rear;
         rear->prev = q;
-        ++theSize;
+        ++siz;
     }
 
-    void add(int index, E e) {
-        assert(index >= 0 && index < theSize);
-        LinkedNode<E> *current = head->next;
-        for (int j = 0; j < index; j++) {
-            current = current->next;
-        }
-        LinkedNode<E> *q = new LinkedNode<E>(e);
-        q->prev = current->prev;
-        q->next = current;
-        current->prev->next = q;
-        current->prev = q;
-        ++theSize;
-    }
-
-    E removeFirst() {
+    E pop_front() {
         assert(!isEmpty());
         LinkedNode<E> *q = head->next;
         q->next->prev = head;
         head->next = q->next;
         E val = q->val;
         delete q;
-        --theSize;
+        --siz;
         return val;
     }
     
-    E removeLast() {
+    E pop_back() {
         assert(!isEmpty());
         LinkedNode<E> *q = rear->prev;
         q->prev->next = rear;
         rear->prev = q->prev;
         E val = q->val;
         delete q;
-        --theSize;
+        --siz;
         return val;
     }
 
-    bool removeFirstOccurrence(E x) {
-        LinkedNode<E> *q;
-        for (q = head->next; q != rear; q = q->next) {
-            if (q->val == x) {
-                q->prev->next = q->next;
-                q->next->prev = q->prev;
-                delete q;
-                --theSize;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool removeLastOccurrence(E x) {
-        LinkedNode<E> *lastNode = NULL;
-        LinkedNode<E> *q;
-        for (q = head->next; q != rear; q = q->next) {
-            if (q->val == x) {
-                lastNode = q;
-            }
-        }
-        if (lastNode) {
-            lastNode->prev->next = lastNode->next;
-            lastNode->next->prev = lastNode->prev;
-            delete lastNode;
-            --theSize;
-        }
-        return lastNode != NULL;
-    }
-
-    /* different from Java API
-     * because of the pitfall of C++ language
-     */
-    E removeByIndex(int index) {
-        assert(index >= 0 && index < theSize);
-        LinkedNode<E> *q = head->next;
-        for (int j = 0; j < index; j++) {
-            q = q->next;
-        }
-        q->prev->next = q->next;
-        q->next->prev = q->prev;
-        E val = q->val;
-        delete q;
-        --theSize;
-        return val;
-    }
-
-    bool remove(E x) {
-        LinkedNode<E> *q;
-        for (q = head->next; q != rear; q = q->next) {
-            if (q->val == x) {
-                q->prev->next = q->next;
-                q->next->prev = q->prev;
-                delete q;
-                --theSize;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void fromArray(Array<E> *array) {
-        clear();
-        for (int i = 0; i < array->size(); i++) {
-            addLast(array->get(i));
-        }
-    }
-
-    // :ABANDONED:
-    //// :IMPROVEABLE: efficiency
-    //void fromArray(E *source, int length) {
-    //    clear();
-    //    for (int i = 0; i < length; i++) {
-    //        add(source[i]);
-    //    }
-    //}
-
-
-    Array<E> *toArray() {
-        E *temp = new E[theSize];
-        for (int i = 0; i < theSize; i++) {
-            temp[i] = get(i);
-        }
-        Array<E> *array = new Array<E>(temp, theSize);
-        delete[] temp;
-        return array;
-    }
-
-    // :ABANDONED:
-    //// :IMPROVEABLE: efficiency
-    //void toArray(E *destination) {
-    //    for (int i = 0; i < size(); i++) {
-    //        destination[i] = get(i);
-    //    }
-    //}
-
-    void input();
-    void output();
+//    void fromArray(Array<E> *array) {
+//        clear();
+//        for (int i = 0; i < array->size(); i++) {
+//            addLast(array->get(i));
+//        }
+//    }
 
 private:
     LinkedNode<E> *head;
     LinkedNode<E> *rear;
-    int theSize;
+    int siz;
 };
-
-template <typename E>
-void LinkedList<E>::input()
-{
-    int len;
-    std::cin >> len;
-    E val;
-    for (int i = 0; i < len; i++) {
-        std::cin >> val;
-        push_back(val);
-    }
-}
-
-template <typename E>
-void LinkedList<E>::output()
-{
-    LinkedNode<E> *q = head->next;
-    for (int i = 0; i < theSize; i++) {
-        if (q == NULL) break;
-        std::cout << q->val << " ";
-        q = q->next;
-    }
-    std::cout << std::endl;
-}
 
 #endif
