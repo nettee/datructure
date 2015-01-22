4969525a6786861f4d614986bd7dee21c269e74b
William <xf0718@gmail.com>
2015-01-22 22:39:43 +0800

    rewrite vector, with iterator

--- include/list/vector.h
+++ include/list/vector.h
@@ -1,153 +1,54 @@
 #ifndef __VECTOR_H__
 #define __VECTOR_H__
 
-#include <iostream>
-#include <cassert>
-
 template <typename E>
 class Vector {
 public:
-    Vector() {
-        capacity = 10;
-        elements = new E[capacity];
-        cap_incr = 0;
-        siz = 0;
-    }
-    Vector(int init_capacity) {
-        capacity = init_capacity;
-        elements = new E[capacity];
-        cap_incr = 0;
-        siz = 0;
-    }
-    Vector(int init_capacity, int increment) {
-        capacity = init_capacity;
-        elements = new E[capacity];
-        cap_incr = increment;
-        siz = 0;
-    }
-    Vector(Vector &vec) {
-        assert(!"Not implemented");
+    typedef E *iterator;
+    typedef const E *const_iterator;
+
+    Vector(): the_size(0) {
+        data = new E[16];
+        capacity = 16;
     }
     
     ~Vector() {
-        delete[] elements;
+        delete[] data;
     }
 
-    void clear() {
-        removeAllElements();
-    }
+    E& operator[](size_t i) { return data[i]; }
+    const E& operator[](size_t i) const { return data[i]; }
 
-    bool empty() {
-        return siz == 0;
-    }
+    iterator begin() { return data; }
+    const_iterator begin() const { return data; }
+    iterator end() { return data + the_size; }
+    const_iterator end() const { return data + the_size; }
 
-    int size() {
-        return siz;
-    }
+    void clear() { the_size = 0; }
+    size_t size() const { return the_size; }
 
-    bool contains(E x) {
-        for (int i = 0; i < siz; i++) {
-            if (elements[i] == x) {
-                return true;
-            }
+    void push_back(const E& x) {
+        if (the_size == capacity) {
+            grow();
         }
-        return false;
-    }
-
-    int indexOf(E x) {
-        for (int i = 0; i < siz; i++) {
-            if (elements[i] == x) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    int lastIndexOf(E x) {
-        int lastIndex = -1;
-        for (int i = 0; i < siz; i++) {
-            if (elements[i] == x) {
-                lastIndex = i;
-            }
-        }
-        return lastIndex;
-    }
-
-    int lastIndexOf(E x, int index) {
-        for (int j = index; j >= 0; j--) {
-            if (elements[j] == x) {
-                return j;
-            }
-        }
-        return -1;
-    }
-
-    E elementAt(int index) {
-        assert(index >= 0 && index < size());
-        return elements[index];
-    }
-
-    E firstElement() {
-        assert(!empty());
-        return elements[0];
-    }
-
-    E lastElement() {
-        assert(!empty());
-        return elements[siz - 1];
-    }
-
-    E setElementAt(E obj, int index) {
-        assert(index >= 0 && index < size());
-        E previousValue = elements[index];
-        elements[index] = element;
-        return previousValue;
-    }
-
-    void addElement(E obj) {
-        assert(siz < capacity);
-        elements[siz++] = obj;
-    }
-
-    void insertElementAt(E obj, int index) {
-        assert(index >= 0 && index < size());
-        for (int j = siz; j > index; j--) {
-            elements[j] = elements[j-1];
-        }
-        siz[index] = obj;
-        ++siz;
-    }
-
-    void removeElementAt(int index) {
-        assert(index >= 0 && index < size());
-        for (int j = index; j < siz - 1; j++) {
-            elements[j] = elements[j+1];
-        }
-        --siz;
-    }
-
-    bool removeElement(E obj) {
-        for (int i = 0; i < siz; i++) {
-            if (elements[i] == obj) {
-                for (int j = i;  < siz - 1; j++) {
-                    elements[j] = elements[j+1];
-                }
-                --siz;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool removeAllElements() {
-        siz = 0;
+        data[the_size++] = x;
     }
 
 private:
-    E *elements;
-    int siz;
-    int capacity;
-    int cap_incr;
+    E *data;
+    size_t the_size;
+    size_t capacity;
+
+    void grow() {
+        int new_cap = capacity * 2;
+        E *new_data = new E[new_cap];
+        for (int i = 0; i < the_size; i++) {
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+        capacity = new_cap;
+    }
 };
 
-#endif 
+#endif
